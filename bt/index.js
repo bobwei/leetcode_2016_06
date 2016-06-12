adeb244fc61ffb6ebb9d3ed45d250ee06f5198c7
bobwei <bob61016@gmail.com>
2016-06-12 18:26:59 +0800

    add .eslinttc, rewrite bt function class to es6 class

--- bt/index.js
+++ bt/index.js
@@ -1,60 +1,53 @@
-export default function TreeNode(val) {
-  this.val = val;
-  this.left = this.right = null;
+export default class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = this.right = null;
+  }
+
+  insert(val) {
+    const queue = [this];
+    while (queue.length) {
+      const node = queue.shift();
+      if (!node.left) {
+        node.left = new TreeNode(val);
+        break;
+      } else if (!node.right) {
+        node.right = new TreeNode(val);
+        break;
+      } else {
+        queue.push(node.left, node.right);
+      }
+    }
+    return this;
+  }
+
+  bfs() {
+    const queue = [this];
+    const output = [];
+    while (queue.length) {
+      const node = queue.shift();
+      output.push(node.val);
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+    return output;
+  }
 }
 
-TreeNode.prototype.insert = function(val) {
-  let queue = [this];
-  while (queue.length) {
-    let node = queue.shift();
-    if (!node.left) {
-      node.left = new TreeNode(val);
-      break;
-    } else if (!node.right) {
-      node.right = new TreeNode(val);
-      break;
-    } else {
-      queue.push(node.left, node.right);
-    }
-  }
-  return this;
-};
-
-TreeNode.prototype.bfs = function() {
-  let queue = [this];
-  let output = [];
-  while (queue.length) {
-    let node = queue.shift();
-    output.push(node.val);
-    if (node.left) {
-      queue.push(node.left);
-    }
-    if (node.right) {
-      queue.push(node.right);
-    }
-  }
-  return output;
-};
-
 if (require.main === module) {
-  let start = 0;
-  let end = 10;
-  let node = new TreeNode(start);
-  Array
+  const start = 0;
+  const end = 10;
+  const rootNode = Array
+    // create [start + 1, ...end], i.e., [1, ...10]
     .from(new Array(end), (x, i) => i + 1)
-    .forEach((x) => {
-      node.insert(x);
-    });
-  console.log(`insert sequentially from ${start} to ${end}`);
-  console.log(node);
-  console.log('');
-
-  let node2 = (new TreeNode(11)).insert(12).insert(13);
-  console.log('chainable insertion test');
-  console.log(node2);
-  console.log('');
-
-  console.log('bfs test');
-  console.log(node.bfs());
-  console.log('');
+    // chainable node => return root node
+    .reduce(
+      (prev, current) => prev.insert(current),
+      new TreeNode(start)
+    );
+  console.log(rootNode.bfs());
 }
